073c875182a5d61687adef4e582f4b40c621b0e5
Priyam Das <priyamdas@gmail.com>
2015-10-04 17:39:51 -0700

    Generalize create_states

--- Markov.py
+++ Markov.py
@@ -1,11 +1,10 @@
 import string
 digs = string.digits
 
-"""The following code to convert an integer to a string in base x 
-is adapted from http://stackoverflow.com/a/2267446
-Meant to be the inverse of the built in function int(x, base) where 
-x must be a string if a base other than 10 is specified."""
 def int2base(x, base):
+	"""Code which converts an integer into a string in base x 
+	Adapted from http://stackoverflow.com/a/2267446
+	Meant as an inverse of the built in function int(x, base)"""
 	if x < 0:
 		sign = -1
 	elif x == 0:
@@ -22,11 +21,19 @@ def int2base(x, base):
 	digits.reverse()
 	return ''.join(digits)
 
-def create_states(boardsize=4):
-	total = 3**boardsize
+
+def create_states(numpieces=3, boardsize=4):
+	"""
+	Returns a list of all possible states represented as bit strings
+
+	numpieces: Number of configurations a particular space can take, 
+			   including the null configuration.
+	boardsize: Number of spaces on the gameboard
+	"""
+	total = numpieces**boardsize
 	states = []
 	for i in range(0, total):
-		state = int2base(i,3)
+		state = int2base(i,numpieces)
 		if len(state) < boardsize:
 			state = (str(0)*(boardsize - len(state))) + state
 		states.append(state)
