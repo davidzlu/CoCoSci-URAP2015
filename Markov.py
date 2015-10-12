6e8b0f5b069e672e6c9a25915cda03b089876462
David Lu <davidzlu@berkeley.edu>
2015-10-12 00:06:37 -0700

    Wrote transition_prob and associated functions, wrote random_policy

--- Markov.py
+++ Markov.py
@@ -1,4 +1,6 @@
 import string
+from TicTacToe import *
+import random
 digs = string.digits
 
 """The following code to convert an integer to a string in base x 
@@ -32,3 +34,129 @@ def create_states(boardsize=4):
 		states.append(state)
 	return states
 
+def state_space():
+	"""Returns a list of all legal states for a 2x2 game of TicTacToe"""
+	states = create_states()
+	invalid = []
+	for item in states:
+		x = 0
+		o = 0
+		#counts numbers of x's and o's to make sure no player skipped another's turn
+		for digit in range(4):
+			if item[digit] == '1':
+				x += 1
+			elif item[digit] == '2':
+				o += 1
+		if (x > o+1) or (o > x+1):
+			invalid.append(item)
+	for item in invalid:
+		states.remove(item)
+	return states
+
+def create_state_tree(states):
+	""" Takes in list of states as base 3 string of digits.
+	Creates dictionary that maps a state to a list of all the ones
+	rechable from it."""
+	state_tree = {}
+	for cur_state in states:
+		state_tree[cur_state] = []
+		for next_state in states:
+			if valid_transition(cur_state, next_state):
+				state_tree[cur_state].append(next_state)
+	return state_tree
+
+def action_space(board):
+    """Returns list of all possible actions for the given board"""
+    actions = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            actions.append([i,j])
+    return actions
+
+def legal_actions(curr_state, actions):
+    """Returns list of legal moves to make on your turn
+    curr_state: bit string representing current game curr_state
+    actions: list of all possible actions
+    """
+    laction_set = []
+    for item in actions:
+        if curr_state[actions.index(item)] == '0':
+            laction_set.append(item)
+    return laction_set
+
+def board2state(board):
+	"""Returns string representation of the board passed in"""
+	s = ''
+	for i in board:
+		for j in i:
+			s += str(j)
+	return s
+
+def valid_transition(cur_state, next_state):
+	if cur_state == next_state:
+		return False
+	num_differs = 0
+	for i in range(0, len(cur_state)):	
+		if cur_state[i] != '0' and next_state[i] != cur_state[i]:
+			return False
+		if cur_state[i] != next_state[i]:
+			num_differs += 1
+	if num_differs == 2:
+		return True
+	return False
+
+def transition_prob(next_state, cur_state, action, state_tree):
+    """ Return probability of transitioning into next_state from
+    cur_state and action. """
+    if next_state in state_tree[cur_state]:
+    	num_rechable_states = 0
+    	for state in state_tree[cur_state]:
+    		space_check = 2*action[0] + action[1]
+    		if state[space_check] == '1':
+    			num_rechable_states += 1
+    	if num_rechable_states > 0:
+    		return 1.0/num_rechable_states
+    return 0
+
+def reward_function(cur_state, action, next_state):
+	expected_reward = 0
+	#if player1 wins, 1 for reward
+	#if player2 wins, -1 for reward
+	#if neither, 0 for reward
+	return None
+
+def simulate_transition():
+
+	return None
+
+def main():
+	board = create_board()
+	player = 1
+	move = [-1, -1]
+	while not check_win(board):
+		if player == 2:
+			while not valid_move(board, move[0], move[1]):
+				move[0] = random.randint(0, 2)
+				move[1] = random.randint(0, 2)
+			board = put_piece(board, move[0], move[1], player)
+			player = 1
+		elif player == 1:
+			#Find current state
+			#Take action
+			#Receive reward, adjust probability of transition
+			player = 2
+			return
+
+#modeling environment, not choosing action
+#policy of human interaction as well
+#after playthrough, output useful information, i.e. states, moves
+
+#filter by min number ratings (100)
+#find games that can be approximated w/ Markov model
+#low, moderately low, middle, mid high, high
+
+if __name__ == "__main__":
+ 	states = state_space()
+ 	state_tree = create_state_tree(states)
+ 	prob = transition_prob('1200', '0000', [0,0], state_tree)
+ 	prob = transition_prob('1212', '1200', [1,0], state_tree)
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,8 @@
 _zip = zip
 
+from Markov import *
+import random
+
 def mean(s):
     """Return the arithmetic mean of a sequence of numbers s.
 
@@ -46,9 +49,76 @@ def put_piece(board, row, column, player):
     x[row][column] = player
     return board
 
+def valid_move(board, row, column):
+    if row < 0 or column < 0:
+        return False
+    return board[row][column] == 0
+
 #cannot find a good way to write so assume the board to be 2*2
 def check_win(board):
-    if board[0][0] == board[0][1] or board[1][0] == board[1][1] or board[0][0] == board[1][0] or board[0][1] == board[1][1]:
+    row_win = check_row_win(board)
+    column_win = check_column_win(board)
+    if row_win[0]:
+        return row_win
+    if column_win[0]:
+        return column_win
+
+def check_row_win(board):
+    """ Takes in board and returns True if a player has won, along with 
+    1 or 2 depending on which player won. """
+    for row in board:
+        player = row[0]
+        num_in_row = 0
+        if player != 0:
+            for space in row:
+                if space == player:
+                    num_in_row += 1
+        if num_in_row == len(row):
+            return True, player
+    return False, 0
+
+def check_column_win(board):
+    """ Takes in board and returns True if a player has won, along with 
+    1 or 2 depending on which player won. """
+    for column in range(0, board.len()):
+        player = board[0][column]
+        num_in_column = 0
+        if player != 0:
+            for row in board:
+                if row[i] == player:
+                    num_in_column += 1
+        if num_in_column == len(board):
+            return True, player
+    return False, 0
+
+def check_tie(board):
+    if board[0][0] != 0 and board[1][0] != 0 and board[0][1] != 0 and board[1][1] != 0:
         return True
     else:
         return False
+
+def random_policy(board):
+    board_str = board2state(board)
+    actions = action_space(board)
+    legal_acts = legal_actions(board_str, actions)
+    if len(legal_acts) > 0:
+        i = random.randint(0, len(legal_acts) - 1)
+        return legal_acts[i]
+    return None
+        
+def other(who):
+    return 3 - who
+
+#human-player phase: we print a comment to ask
+def play(strategy1, strategy2):
+    who = 1
+    board = create_board()
+    while not check_win and not check_tie:# try make a list of tuple for every move and return it with final
+        if who == 1:
+            row, column = strategy1(board)[0], strategy1(board)[1]
+            board = put_piece(board, row, column, who)
+        if who == 2:
+            row, column = strategy2(board)[0], strategy2(board)[1]
+            board = put_piece(board, row, column, who)
+        who = other(who)
+    return board
