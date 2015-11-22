3316cf573f80589ab6981834530d67b64bf29d1d
Priyam Das <priyamdas@gmail.com>
2015-11-22 12:54:58 -0800

    fixed syntax and tested take_action, possible_actions

--- peg_markov.py
+++ peg_markov.py
@@ -11,13 +11,13 @@ def board2state(board):
 		state += ''.join(map(str, board[row, :]))
 	return state
 
-#def possible_actions(board):
-#	actions = []
-#	for i in range(len(board)):
-#        for j in range(len(board)):
-#			if board[i][j] = 0:
-#				actions.append([i, j, 1])
-#				actions.append([i, j, 2])
-#				actions.append([i, j, 3])
-#				actions.append([i, j, 4])
-#	return actions
+def possible_actions(board):
+	actions = []
+	for i in range(len(board)):
+		for j in range(len(board)):
+			if board[i][j] == 0:
+				actions.append([i, j, 1])
+				actions.append([i, j, 2])
+				actions.append([i, j, 3])
+				actions.append([i, j, 4])
+	return actions
--- peg_solitaire.py
+++ peg_solitaire.py
@@ -10,21 +10,21 @@ def create_board():
 	board[3, 3] = 0
 	return board.astype(int)
 
-#def take_action(board, endrow, endcolumn, direction):
-#	1 = down 2 = right 3 = up 4 = left
-#	assert direction = 1 or direction = 2 or direction = 3 or direction = 4
-#	x = board[:][:]
-#	x[endrow][endcolumn] = 1
-#	if direction = 1:
-#		x[endrow + 1][endcolumn] = 0
-#		x[endrow + 2][endcolumn] = 0
-#	elif direction = 2:
-#		x[endrow][endcolumn - 1] = 0
-#		x[endrow][endcolumn - 2] = 0
-#	elif direction = 3:
-#		x[endrow - 1][endcolumn] = 0
-#		x[endrow - 2][endcolumn] = 0
-#	elif direction = 4:
-#		x[endrow][endcolumn + 1] = 0
-#		x[endrow][endcolumn + 2] = 0
-#	return x
+def take_action(board, endrow, endcolumn, direction):
+	# 1 = down 2 = right 3 = up 4 = left
+	assert direction == 1 or direction == 2 or direction == 3 or direction == 4
+	x = board[:][:]
+	x[endrow][endcolumn] = 1
+	if direction == 1:
+		x[endrow + 1][endcolumn] = 0
+		x[endrow + 2][endcolumn] = 0
+	elif direction == 2:
+		x[endrow][endcolumn - 1] = 0
+		x[endrow][endcolumn - 2] = 0
+	elif direction == 3:
+		x[endrow - 1][endcolumn] = 0
+		x[endrow - 2][endcolumn] = 0
+	elif direction == 4:
+		x[endrow][endcolumn + 1] = 0
+		x[endrow][endcolumn + 2] = 0
+	return x
