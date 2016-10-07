63ad930b348e0362fb14e5ddb99a22d604cdc279
Jekyll1021 <jerry_1021@msn.cn>
2016-10-07 14:09:57 -0700

    InfantryForce

--- TAL_battalions.py
+++ TAL_battalions.py
@@ -15,17 +15,29 @@ def get_enemy_units(list_unittype_num):
 class MobileHQ:
 	def __init__(self):
 		self.vp = 2
-		self.place = (3, "C")
+		self.type = (3, "C")
 		self.units = get_enemy_units([["AAA", 2], ["APC", 2], ["Commands", 4], ["SCUD", 2], ["SPA", 2], ["Truck", 2]])
 		self.half_value = 16
 		self.destroy_value = 5
 		"""TODO list: special note here"""
 
+class InfantryForce:
+	def __init__(self):
+		self.vp = 5
+		self.type = (1, "A")
+		self.units = get_enemy_units([["AAA", 4], ["APC", 5], ["Commands", 2], ["Infantry", 10], ["Truck", 4]])
+		self.half_value = 20
+		self.destroy_value = 5
 
 class TestMethods(unittest.TestCase):
 	def test_get_enemy_units_mobile_HQ(self):
 		self.assertTrue(MobileHQ().units == ["AAA", "AAA", "APC", "APC", "Commands", 
 			"Commands", "Commands", "Commands", "SCUD", "SCUD", "SPA", "SPA", "Truck", "Truck"])
+
+	def test_get_enemy_units_InfantryForce(self):
+		self.assertTrue(InfantryForce().units == ["AAA", "AAA", "AAA", "AAA", "APC", "APC", "APC", "APC", "APC",
+			"Commands", "Commands", "Infantry", "Infantry", "Infantry", "Infantry", "Infantry", "Infantry", "Infantry",
+			"Infantry", "Infantry", "Infantry", "Truck", "Truck", "Truck", "Truck"])
 
 
 if __name__ == '__main__':
