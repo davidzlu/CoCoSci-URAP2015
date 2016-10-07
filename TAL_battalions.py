364ac25c7bac72090a0dc4ea7baad1c92a8b67f8
Jekyll1021 <jerry_1021@msn.cn>
2016-10-07 11:42:01 -0700

    fix last commit

--- TAL_battalions.py
+++ TAL_battalions.py
@@ -3,3 +3,17 @@ from TAL_terrain import *
 from TAL_campaigns import *
 from TAL_situation import *
 
+def get_enemy_units(list_unittype_num):
+	enemy_unit = []
+	for element in list_unittype_num:
+		for i in range(0, element[1]):
+			enemy_unit.append(element[0])
+	return enemy_unit
+
+class MobileHQ:
+	self.vp = 2
+	self.place = (3, "C")
+	self.units = get_enemy_units([["AAA", 2], ["APC", 2], ["Commands", 4], ["SCUD", 2], ["SPA", 2], ["Truck", 2]])
+	self.half_value = 16
+	self.destroy_value = 5
+
