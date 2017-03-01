c90e3e84999a39bc3cd828abba4888cf7c3c851b
Priyam Das <priyamdas@gmail.com>
2017-03-01 00:06:27 -0800

    added weapon set to planes

--- deck_building/tal/TAL_planes.py
+++ deck_building/tal/TAL_planes.py
@@ -3,241 +3,243 @@ import unittest
 from deck_building.tal import TAL_campaigns, TAL_situation
 
 class Plane:
-	"""Abstract class for planes. Defines instance variables and
-	common methods.
-	"""
-	def __init__(self):
-		self.year = 0
-		self.cannon = 0
-		self.so = 0
-		self.hit = 0
-		self.weapons_equipped = [] # The weapons the agent chose to equip to plane
-		self.weapon_set = set() #
-		self.weight = 0
-		self.special = None
-		
-	def get_year(self):
-		return self.year
-	
-	def get_cannon(self):
-		return self.cannon
-	
-	def get_cost(self):
-		return self.so
-	
-	def get_hitpoints(self):
-		return self.hit
-	
-	def get_weapons_equip(self):
-		return self.weapons_equipped
-	
-	def get_weapon_set(self):
-		return self.weapon_set
-	
-	def get_weight(self):
-		return self.weight
-		
-		
+    """Abstract class for planes. Defines instance variables and
+    common methods.
+    """
+    def __init__(self):
+        self.year = 0
+        self.cannon = 0
+        self.so = 0
+        self.hit = 0
+        self.weapons_equipped = [] # The weapons the agent chose to equip to plane
+        self.weapon_set = set() #
+        self.weight = 0
+        self.special = None
+
+    def get_year(self):
+        return self.year
+
+    def get_cannon(self):
+        return self.cannon
+
+    def get_cost(self):
+        return self.so
+
+    def get_hitpoints(self):
+        return self.hit
+
+    def get_weapons_equip(self):
+        return self.weapons_equipped
+
+    def get_weapon_set(self):
+        return self.weapon_set
+
+    def get_weight(self):
+        return self.weight
+
+
 class A_10A(Plane):
-	def __init__(self):
-		self.year = 1976
-		self.cannon = 4
-		self.so = 8
-		self.hit = 3
-		"""TODO: weapon list"""
-		self.weight = 14
+    def __init__(self):
+        self.year = 1976
+        self.cannon = 4
+        self.so = 8
+        self.hit = 3
+        self.weapon_set = {'AIM_9', 'AIM_92', 'MK_20', 'LAU_61', 'LAU_68', 'AGM_114', 'AGM_65', 'MK_82', 'MK_83',
+                           'GBU_12', 'GBU_16', 'ECM Pod', 'Fuel Tank'}
+        self.weight = 14
 
 class AH_1(Plane):
-	def __init__(self):
-		self.year = 1967
-		self.cannon = 9
-		self.so = 2
-		self.hit = 1
-		"""TODO: weapon list"""
-		self.weight = 6
+    def __init__(self):
+        self.year = 1967
+        self.cannon = 9
+        self.so = 2
+        self.hit = 1
+        self.weapon_set = {'AIM_9', 'AIM_92', 'BGM_71', 'LAU_61', 'LAU_68', 'AGM_114', 'Fuel Tank'}
+        self.weight = 6
 
 class F_16(Plane):
-	def __init__(self):
-		self.year = 1976
-		self.cannon = 7
-		self.so = 5
-		self.hit = 2
-		"""TODO: weapon list"""
-		self.weight = 10
+    def __init__(self):
+        self.year = 1976
+        self.cannon = 7
+        self.so = 5
+        self.hit = 2
+        self.weapon_set = {'AIM_9', 'MK_20', 'LAU_61', 'LAU_68', 'AGM_65', 'MK_82', 'MK_83', 'GBU_12',
+                           'GBU_16', 'ECM Pod', 'Fuel Tank'}
+        self.weight = 10
 
 class AH_64A(Plane):
-	def __init__(self):
-		self.year = 1986
-		self.cannon = 7
-		self.so = 4
-		self.hit = 2
-		"""TODO: weapon list"""
-		self.weight = 8
+    def __init__(self):
+        self.year = 1986
+        self.cannon = 7
+        self.so = 4
+        self.hit = 2
+        self.weapon_set = {'AIM_9', 'AIM_92', 'LAU_61', 'LAU_68', 'AGM_114', 'Fuel Tank'}
+        self.weight = 8
 
 class AV_8B(Plane):
-	def __init__(self):
-		self.year = 1985
-		self.cannon = 7
-		self.so = 6
-		self.hit = 2
-		"""TODO: weapon list"""
-		self.weight = 10
+    def __init__(self):
+        self.year = 1985
+        self.cannon = 7
+        self.so = 6
+        self.hit = 2
+        self.weapon_set = {'AIM_9', 'AIM_92', 'MK_20', 'LAU_61', 'LAU_68', 'AGM_65', 'AGM_114', 'MK_82', 'MK_83', 'GBU_12',
+                           'GBU_16', 'ECM Pod', 'Fuel Tank'}
+        self.weight = 10
 
 class A_10C(Plane):
-	def __init__(self):
-		self.year = 2006
-		self.cannon = 4
-		self.so = 9
-		self.hit = 3
-		"""TODO: weapon list"""
-		self.weight = 14
+    def __init__(self):
+        self.year = 2006
+        self.cannon = 4
+        self.so = 9
+        self.hit = 3
+        self.weapon_set = {'AIM_9', 'AIM_92', 'MK_20', 'LAU_61', 'LAU_68', 'AGM_65', 'AGM_114', 'MK_82', 'MK_83', 'GBU_12',
+                           'GBU_16', 'ECM Pod', 'Fuel Tank'}
+        self.weight = 14
 
 class MQ_1(Plane):
-	def __init__(self):
-		self.year = 2001
-		self.cannon = 0
-		self.so = 4
-		self.hit = 0
-		"""TODO: weapon list"""
-		self.weight = 0
+    def __init__(self):
+        self.year = 2001
+        self.cannon = 0
+        self.so = 4
+        self.hit = 0
+        self.weapon_set = {'AGM_114'}
+        self.weight = 0
 
 class RQ_1(Plane):
-	def __init__(self):
-		self.year = 1995
-		self.cannon = 0
-		self.so = 4
-		self.hit = 0
-		"""TODO: weapon list"""
-		self.weight = 0
+    def __init__(self):
+        self.year = 1995
+        self.cannon = 0
+        self.so = 4
+        self.hit = 0
+        self.weight = 0
 
 class AH_64D(Plane):
-	def __init__(self):
-		self.year = 1997
-		self.cannon = 7
-		self.so = 5
-		self.hit = 2
-		"""TODO: weapon list"""
-		self.weight = 8
+    def __init__(self):
+        self.year = 1997
+        self.cannon = 7
+        self.so = 5
+        self.hit = 2
+        self.weapon_set = {'AIM_9', 'AIM_92', 'LAU_61', 'LAU_68', 'AGM_114', 'Fuel Tank'}
+        self.weight = 8
 
 class AC_130(Plane):
-	def __init__(self):
-		self.year = 1995
-		self.cannon = 5 # or 3 or 1
-		self.so = 10
-		self.hit = 3
-		"""TODO: special effect"""
-		self.weight = 0
+    def __init__(self):
+        self.year = 1995
+        self.cannon = 5 # or 3 or 1
+        self.so = 10
+        self.hit = 3
+        """TODO: special effect"""
+        self.weight = 0
 
 plane_pool = {A_10A: 4, AH_1: 3, F_16: 1, AH_64A: 8, AV_8B: 3, A_10C: 2, MQ_1: 2, RQ_1: 2, AH_64D: 4, AC_130: 1}
 
 def legal_actions(campaign):
-	possible_choices = []
-	for plane in plane_pool.keys():
-		if plane().year <= campaign.year and plane_pool[plane] > 0:
-			possible_choices.append(plane)
-	return possible_choices
+    possible_choices = []
+    for plane in plane_pool.keys():
+        if plane().year <= campaign.year and plane_pool[plane] > 0:
+            possible_choices.append(plane)
+    return possible_choices
 
 def get_all_planes(campaign, situation, strategy):
-	planes = []
-	to_pick_or_not = True
-	while to_pick_or_not:
-		planes_so = 0
-		if planes != None:
-			planes_so = sum([p.so for p in planes])
-		if planes_so >= situation.SOpoints:
-			break
-		plane = strategy(legal_actions(campaign))
-		planes.append(plane())
-		"""TODO: implement methods to determine continue pick plane or not """
-		to_pick_or_not = strategy(situation.SOpoints - planes_so)
-	return planes
+    planes = []
+    to_pick_or_not = True
+    while to_pick_or_not:
+        planes_so = 0
+        if planes != None:
+            planes_so = sum([p.so for p in planes])
+        if planes_so >= situation.SOpoints:
+            break
+        plane = strategy(legal_actions(campaign))
+        planes.append(plane())
+        """TODO: implement methods to determine continue pick plane or not """
+        to_pick_or_not = strategy(situation.SOpoints - planes_so)
+    return planes
 
 def random_policy(possible_choices):
-	return random.choice(possible_choices)
+    return random.choice(possible_choices)
 
 class Weapon:
-	def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, VB, independence):
-		self.weaponPoints = weaponPoints
-		self.ordnancePoints = ordnancePoints
-		self.attackNumber = attackNumber
-		self.attackRange = attackRange
-		# A tuple of number interval to specify the range
-		self.altitudeAttacks = altitudeAttacks
-		# a list specifying which height to attack, in form of [boolean, boolean] correspond to {high, low}
-		self.VB = VB
-		# Vehicle/building or not, in form of boolean
-		self.independence = independence
-		# independent or not, boolean
-		###for detailed discription see weapon section in rule book
-	def attack(self, target, dice_roll):
-		if dice_roll >= self.attackNumber:
-			return
-			# TODO: fill in this method to enable attack
-		return
+    def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, VB, independence):
+        self.weaponPoints = weaponPoints
+        self.ordnancePoints = ordnancePoints
+        self.attackNumber = attackNumber
+        self.attackRange = attackRange
+        # A tuple of number interval to specify the range
+        self.altitudeAttacks = altitudeAttacks
+        # a list specifying which height to attack, in form of [boolean, boolean] correspond to {high, low}
+        self.VB = VB
+        # Vehicle/building or not, in form of boolean
+        self.independence = independence
+        # independent or not, boolean
+        ###for detailed description see weapon section in rule book
+    def attack(self, target, dice_roll):
+        if dice_roll >= self.attackNumber:
+            target.status = False # TODO: check what the target attributes actually are
+        # removing weapon from set will be a separate method
 
 class VB_Weapon(Weapon):
-	def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks):
-		Weapon.__init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, True, False)
-	
-	def attack(self, target, dice_roll):
-		if dice_roll >= self.attackNumber:
-			return
-			# TODO: fill in this method to enable attack
-		return
+    def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks):
+        Weapon.__init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, True, False)
+
+    def attack(self, target, dice_roll):
+        if dice_roll >= self.attackNumber:
+            return
+            # TODO: fill in this method to enable attack
+        return
 
 class Independent_Weapon(Weapon):
-	def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks):
-		Weapon.__init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, False, True)
+    def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks):
+        Weapon.__init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, False, True)
 
-	def attack(self, target, dice_roll):
-		if dice_roll >= self.attackNumber:
-			return
-			# TODO: fill in this method to enable attack
-		return
+    def attack(self, target, dice_roll):
+        if dice_roll >= self.attackNumber:
+            return
+            # TODO: fill in this method to enable attack
+        return
 
 class Ordinary_Weapon(Weapon):
-	def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks):
-		Weapon.__init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, False, False)
+    def __init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks):
+        Weapon.__init__(self, weaponPoints, ordnancePoints, attackNumber, attackRange, altitudeAttacks, False, False)
 
-	def attack(self, target, dice_roll):
-		if dice_roll >= self.attackNumber:
-			return
-			# TODO: fill in this method to enable attack
-		return
+    def attack(self, target, dice_roll):
+        if dice_roll >= self.attackNumber:
+            return
+            # TODO: fill in this method to enable attack
+        return
 
 class MK_83(Ordinary_Weapon):
-	def __init__(self):
-		Weapon.__init__(2, 0, 4, (0, 0), [True, True])
+    def __init__(self):
+        Weapon.__init__(2, 0, 4, (0, 0), [True, True])
 
 class LAU_61(Ordinary_Weapon):
-	def __init__(self):
-		Weapon.__init__(2, 1, 4, (0, 1), [True, True])
+    def __init__(self):
+        Weapon.__init__(2, 1, 4, (0, 1), [True, True])
 
 class AGM_114(VB_Weapon):
-	def __init__(self):
-		Weapon.__init__(1, 1, 4, (1, 2), [True, True])
+    def __init__(self):
+        Weapon.__init__(1, 1, 4, (1, 2), [True, True])
 
 class GBU_12(Independent_Weapon):
-	def __init__(self):
-		Weapon.__init__(1, 1, 4, (0, 1), [True, False])
+    def __init__(self):
+        Weapon.__init__(1, 1, 4, (0, 1), [True, False])
 
 class AGM_65(VB_Weapon):
-	def __init__(self):
-		Weapon.__init__(2, 1, 1, (1, 3), [True, True])
+    def __init__(self):
+        Weapon.__init__(2, 1, 1, (1, 3), [True, True])
 
 class TestMethods(unittest.TestCase):
-	def test_legal_actions(self):
-		test_campaign = TAL_campaigns.Iraq()
-		for plane in legal_actions(test_campaign):
-			self.assertTrue(plane().year <= 1991)
+    def test_legal_actions(self):
+        test_campaign = TAL_campaigns.Iraq()
+        for plane in legal_actions(test_campaign):
+            self.assertTrue(plane().year <= 1991)
 
-	def test_get_all_planes(self):
-		test_campaign = TAL_campaigns.Iraq()
-		test_situation = TAL_situation.Surge()
-		sample = get_all_planes(test_campaign, test_situation, random_policy, random_policy)
-		for plane in sample:
-			self.assertTrue(plane.year <= 1991)
-		self.assertTrue(sum([p.so for p in sample]) <= 38)
+    def test_get_all_planes(self):
+        test_campaign = TAL_campaigns.Iraq()
+        test_situation = TAL_situation.Surge()
+        sample = get_all_planes(test_campaign, test_situation, random_policy, random_policy)
+        for plane in sample:
+            self.assertTrue(plane.year <= 1991)
+        self.assertTrue(sum([p.so for p in sample]) <= 38)
 
 if __name__ == '__main__':
     unittest.main()
